8553c1bf3c71d18611decf5a398589449dccc487
gauloish <125079293+gauloish@users.noreply.github.com>
2026-08-04 21:25:45 -0300

    feat: add a session to geoenconding database

---
+++ src/database/session.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Engine
+from sqlalchemy.orm import sessionmaker
+
+from .engine import GeoencodingEngine
+
+
+class GeoencodingSession:
+    def __init__(self, engine: GeoencodingEngine):
+        self.session = sessionmaker(
+            bind=engine.get_engine(),
+            autoflush=False,
+            autocommit=False,
+        )
+
+    def __call__(self):
+        return self.session()
