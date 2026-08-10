b6c8462c5439cf9e1a97e2c6ba02b047c896c6f6
gauloish <125079293+gauloish@users.noreply.github.com>
2026-08-10 06:15:43 -0300

    refactor: use nominatim server instead photon server to geocoding

--- scripts/geocoder/start.sh
+++ scripts/geocoder/start.sh
@@ -10,34 +10,51 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 PROJECT_ROOT="$(cd "$SCRIPT_DIR/../.." && pwd)"
 
 ARTIFACTS_DIR="$PROJECT_ROOT/artifacts/geocoder"
-PHOTON_DIR="$ARTIFACTS_DIR/photon"
-DB_DIR="$PHOTON_DIR"
-JAR_FILE="$PHOTON_DIR/photon.jar"
+NOMINATIM_DIR="$ARTIFACTS_DIR/nominatim"
+OSM_FILE="$NOMINATIM_DIR/goiania.osm.pbf"
 
 source "$SCRIPT_DIR/../utils.sh"
 
 # ------------------------------------------------------------------------------
 
-log_info "Iniciando atualização do Geocoder."
+log_info "Iniciando atualização do Geocoder (Nomantim)."
 
-log_info "[1/3] Checando executável do Photon."
 
-if [ ! -f "$JAR_FILE" ]; then
-    log_error "Executável do Photon não encontrado em: $JAR_FILE."
+log_info "[1/4] Checando pasta do Nominatim."
+
+if [ ! -d "$NOMINATIM_DIR" ]; then
+    log_error "Pasta do Nominatim não encontrada em: $NOMINATIM_DIR."
     log_info "Execute 'bash scripts/setup_geocoder.sh' primeiro."
     exit 1
 fi
 
-log_info "[2/3] Checando banco de dados do mapa do Photon."
 
-if [ ! -d "$DB_DIR" ]; then
-    log_error "Banco de dados do mapa não encontrado em: $DB_DIR."
+log_info "[2/4] Checando o arquivo do mapa de Goiânia."
+
+if [ ! -f "$OSM_FILE" ]; then
+    log_error "Arquivo do mapa de Goiânia não encontrado em: $OSM_FILE."
     log_info "Execute 'bash scripts/setup_geocoder.sh' primeiro."
     exit 1
 fi
 
-log_info "[3/3] Iniciando o servidor de Geocodificação (Photon)."
 
-exec java -jar "$JAR_FILE" -data-dir "$DB_DIR" serve
+log_info "[3/4] Limpando o contêiner do antigo servidor Nomantim."
+
+if docker ps -a --format '{{.Names}}' | grep -Eq "^nominatim\$"; then
+    log_info "Limpando contêiner antigo."
+    docker rm -f nominatim > /dev/null
+fi
+
+
+log_info "[4/4] Iniciando o servidor de Geocodificação (Nominatim)."
+
+docker run -it --shm-size=1g \
+    -e PBF_PATH=/nominatim/data/goiania.osm.pbf \
+    -e NOMINATIM_PASSWORD=password\
+    -v "$NOMINATIM_DIR":/nominatim/data:Z \
+    -v nominatim-data:/var/lib/postgresql/16/main \
+    -p 8080:8080 \
+    --name nominatim \
+    mediagis/nominatim:5.3
 
 log_sucess "API disponível em: http://localhost:2322/api."
